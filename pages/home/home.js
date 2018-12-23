dfd6df6ec576f5d2a93704597d2af8ca77bbead7
panhj <1159688878@qq.com>
2018-12-24 01:31:49 +0800

    Initial Commit

---
+++ pages/home/home.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    coupons: [
+      { num: 20, sum: 199 },
+      { num: 40, sum: 299 },
+      { num: 60, sum: 499 },
+      { num: 20, sum: 199 },
+      { num: 20, sum: 199 },
+    ]
+  }
+})
