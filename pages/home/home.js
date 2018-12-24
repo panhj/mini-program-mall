688600d1fc5f48171c9f0146ac873ac09179f827
panhj <1159688878@qq.com>
2018-12-25 00:16:45 +0800

    ui

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,8 +4,8 @@ Page({
       { num: 20, sum: 199 },
       { num: 40, sum: 299 },
       { num: 60, sum: 499 },
-      { num: 20, sum: 199 },
-      { num: 20, sum: 199 },
+      { num: 80, sum: 699 },
+      { num: 99, sum: 899 },
     ]
   }
 })
