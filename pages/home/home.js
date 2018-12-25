a8a529841cdde0e030023def85e81f6075414a6c
panhj <1159688878@qq.com>
2018-12-26 00:45:08 +0800

    homepage

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,6 +6,14 @@ Page({
       { num: 60, sum: 499 },
       { num: 80, sum: 699 },
       { num: 99, sum: 899 },
+    ],
+    types: [
+      { type: '高跟鞋', en: 'HIGH HEELED' },
+      { type: '平底鞋', en: 'FLAT SHOES' },
+      { type: '凉鞋', en: 'SANDALS' },
+      { type: '坡跟鞋', en: 'WEDGES' },
+      { type: '运动鞋', en: 'SPORTS' },
+      { type: '靴子', en: 'BOOTS' },
     ]
   }
 })
